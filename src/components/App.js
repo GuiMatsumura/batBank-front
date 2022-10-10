2527153542a3880af0c8aa7a02d4b68224040e09
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-10-10 14:42:33 -0300

    :sparkles: feat: add deposit screen to app

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import RenderLogin from './authRoutes/LoginScreen.js';
 import TokenContext from './context/Token';
 import RenderSignUp from './authRoutes/SignUpScreen';
 import RenderHome from './Home';
+import RenderDeposit from './balanceRoutes/DepositScreen';
 
 export default function App() {
   const [token, setToken] = React.useState('');
@@ -17,6 +18,7 @@ export default function App() {
           <Route path="/" element={<RenderLogin />} />
           <Route path="/signup" element={<RenderSignUp />} />
           <Route path="/home" element={<RenderHome />} />
+          <Route path="/deposit" element={<RenderDeposit />} />
         </Routes>
       </BrowserRouter>
     </TokenContext.Provider>
