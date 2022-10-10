9accbeff6f7c869a4aa578cb52ba554d01cf8284
GuiMatsumura <guilhermepenamatsumura@gmail.com>
2022-10-10 11:38:49 -0300

    :sparkles: feat: add login screen to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import '../css/reset.css';
+import RenderLogin from './authRoutes/LoginScreen.js';
 
 export default function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RenderLogin />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
